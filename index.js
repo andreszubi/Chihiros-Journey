22c51a2d5fe1ef0a58a0302c19ba582ef1a9a22b
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-12-08 18:14:29 +0100

    chaneg background speed

--- index.js
+++ index.js
@@ -222,8 +222,8 @@ const drawSpecialFood = (currentSpecialX, currentSpecialY) => {
 const backgroundDraw = () => {
     ctx.drawImage(background, bgx, 0, canvas.width, canvas.height);
     ctx.drawImage(background2, bgx2, 0, canvas.width, canvas.height);
-    bgx -= 4
-    bgx2 -= 4
+    bgx -= 6
+    bgx2 -= 6
 if (bgx < -canvas.width) {
   bgx = canvas.width;
 } 
